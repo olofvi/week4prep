5d7e4960a95690cd466c2d7ece7d35f3599d4822
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-16 10:44:48 +0200

    original commit

---
+++ java_ex1.js
@@ -0,0 +1,18 @@
+var numbers = [1,3,5];
+
+console.log(numbers[2]);
+
+/*What data type is 123/12? "Things in quotes!"? undefined?
+123/12 is a function, "Things in quotes!" is a string, undefined is notthere*/
+
+var hungry=true
+if (hungry)
+  console.log('find next restaurant');
+
+var emptyArray = []
+newLength = emptyArray.push('Olof','Magnus','Andrea')
+
+var person 
+function person ('Olof','Vignisdottir') {
+  console.log(Olof)
+}
