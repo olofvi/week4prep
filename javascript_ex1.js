976ec50ab6b840ead5cf79a933d9d9b63f9f0d49
Olof Vignisdottir <30499007+olofvi@users.noreply.github.com>
2017-08-16 11:25:48 +0200

    changed file name

---
+++ javascript_ex1.js
@@ -0,0 +1,19 @@
+var numbers = [1,3,5];
+
+console.log(numbers[2]);
+
+/*What data type is 123/12? "Things in quotes!"? undefined?
+123/12 is a function, "Things in quotes!" is a string, undefined is notthere*/
+
+var hungry=true
+if (hungry)
+  console.log('find next restaurant');
+
+var emptyArray = []
+newLength = emptyArray.push('Olof','Magnus','Andrea')
+
+funtion person(firstName, lastName) {
+  console.log(firstName);
+}
+
+person('Olof', 'Vignisdottir')
