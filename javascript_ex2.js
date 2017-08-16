321f83b49f96222c40138e0c5c6d0ce32cde7031
Olof Vignisdottir <30499007+olofvi@users.noreply.github.com>
2017-08-16 13:11:47 +0200

    exercise 2

---
+++ javascript_ex2.js
@@ -0,0 +1,9 @@
+function person(firstName,lastName) {
+  console.log(person(firstName));
+}
+input= 'Olof'
+
+function person(firstName, lastName) {
+  console.log(person(firstName + lastName))
+}
+input= 'Olof'+' Vignisdottir'
